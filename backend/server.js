7bb63c6f961aeab3f6e20576360105299e23a53c
Pavan Kumar Settem <88257205+PavanSETTEM-003@users.noreply.github.com>
2023-07-22 08:33:34 +0530

    multi player sockets

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const morgan = require("morgan");
 const path = require("path");
 const bodyParser = require("body-parser");
+const http = require("http");
+const { Server } = require("socket.io");
 require("dotenv").config();
 
 const app = express();
@@ -15,6 +17,14 @@ app.use(morgan("tiny"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 mongoose
   .connect(process.env.Mongo_db_connection_string, {
@@ -114,3 +124,28 @@ app.post("/login", async (req, res) => {
     res.status(500).send("An error occurred during login");
   }
 });
+
+// multiplayer mode
+// Array to store the messages
+const messages = [];
+
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
+
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+
+    // Save the message to the messages array
+    saveMessage(data);
+  });
+});
+
+const saveMessage = (messageData) => {
+  messages.push(messageData);
+  console.log("Message saved to messages array:", messageData);
+  console.log(messages)
+};
